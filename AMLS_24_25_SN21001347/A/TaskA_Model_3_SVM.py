8deefe024ad5851e5a686b1cc7e4792e0e7b92b2
Asyraf Zulkifli <asyrafhakimzulkifli@gmail.com>
2025-01-20 02:22:53 +0000

    Adjusted hyperparameters

--- AMLS_24_25_SN21001347/A/TaskA_Model_3_SVM.py
+++ AMLS_24_25_SN21001347/A/TaskA_Model_3_SVM.py
@@ -1,11 +1,9 @@
-# import libraries
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report,accuracy_score
 from TaskA_utils import BreastMNISTDataset
 from sklearn.preprocessing import MinMaxScaler
 
+#Step 1: Load and preprocess data
 train_data = BreastMNISTDataset('train')
 val_data = BreastMNISTDataset('val')
 test_data = BreastMNISTDataset('test')
@@ -15,15 +13,15 @@ y_train = train_data.labels.ravel()
 x_test = test_data.images.reshape(len(test_data.images), -1)/255.0
 y_test = test_data.labels.ravel()
 
-scaler = MinMaxScaler()
+scaler = MinMaxScaler() #Scale data using MinMaxScaler
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-#Initialise and train model
-svc = SVC(gamma=0.001, C=1, kernel='linear')
+#Step 2: Train SVM
+svc = SVC(gamma='scale', C=1, kernel='linear', class_weight={0: 1, 1: 399/147})
 svc.fit(x_train, y_train)
+
+#Step 3: Evaluate the model
 y_pred = svc.predict(x_test)
 
 print(f'Accuracy: {accuracy_score(y_test,y_pred):.4f} \n{classification_report(y_test, y_pred)}')
-
-print(f"Mean of the training data: {np.mean(x_train):.4f}, Standard deviation of the training data: {np.std(x_train):.4f}")
