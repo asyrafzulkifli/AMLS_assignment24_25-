98b25eb0971315c5cf0201f6b2f234337fb56876
Asyraf Zulkifli <asyrafhakimzulkifli@gmail.com>
2025-01-21 22:31:11 +0000

    Used GridCV to find hyperparameters

--- AMLS_24_25_SN21001347/A/TaskA_Model_4_RF.py
+++ AMLS_24_25_SN21001347/A/TaskA_Model_4_RF.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from TaskA_utils import BreastMNISTDataset
 from sklearn.preprocessing import RobustScaler
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, ADASYN
 
 # Step 1: Load and preprocess data
 train_data = BreastMNISTDataset('train')
@@ -19,12 +19,26 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
 # Step 2: Apply SMOTE for class imbalance
-smote = SMOTE(random_state=42)
+smote = ADASYN(random_state=42)
 x_train_resampled, y_train_resampled = smote.fit_resample(x_train, y_train)
 
+best_param = {
+    'max_depth': 20,
+    'max_features': 'log2',
+    'min_samples_leaf': 1,
+    'min_samples_split': 2,
+    'n_estimators': 300}
+
 # Step 3: Train Random Forest Classifier
-rf = RandomForestClassifier(random_state=42, class_weight='balanced')
-rf.fit(x_train_resampled, y_train_resampled.ravel())
+rf = RandomForestClassifier(
+    random_state=42, class_weight='balanced',
+    max_depth=best_param['max_depth'],
+    max_features=best_param['max_features'],
+    min_samples_leaf=best_param['min_samples_leaf'],
+    min_samples_split=best_param['min_samples_split'],
+    n_estimators=best_param['n_estimators']
+    )
+rf.fit(x_train_resampled, y_train_resampled)
 
 # Evaluate the model
 y_pred = rf.predict(x_test)
