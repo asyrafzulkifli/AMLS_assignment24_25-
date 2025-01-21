897e24bb5030940e84cf4b53c09a1ff47620ee9f
Asyraf Zulkifli <asyrafhakimzulkifli@gmail.com>
2025-01-21 22:31:38 +0000

    Added save_csv

--- AMLS_24_25_SN21001347/A/TaskA_utils.py
+++ AMLS_24_25_SN21001347/A/TaskA_utils.py
@@ -4,7 +4,9 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 import random
-
+import time
+from torch.utils.data import DataLoader
+import csv
 
 # Get the absolute path of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,7 +20,7 @@ class BreastMNISTDataset(Dataset):
         # Load .npz file
         data = np.load(data_path)
         self.images = data[type + '_images']  # Images (Shape: (N, H, W))
-        self.labels = data[type + '_labels']  # Labels (0 or 1)
+        self.labels = data[type + '_labels'].ravel()  # Labels (0 or 1)
         self.transform = transform
 
     def __len__(self):
@@ -59,7 +61,25 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True  # Ensure deterministic behavior
     torch.backends.cudnn.benchmark = False  # Disable benchmarking for reproducibility
 
-train_data = BreastMNISTDataset('train')
+def save_csv(filename,data,header=None):
+    file_path = os.path.join(script_dir, f"./Results/{filename}.csv")
+    with open(file_path, mode='w',newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        for i in range(len(data)):
+            writer.writerow(data[i])
 
-unique, counts = np.unique(train_data.labels, return_counts=True)
-print(unique, counts)
+if __name__ == "__main__":
+    train_data = BreastMNISTDataset('train')
+    dataset = BreastMNISTDataset('train', transform=None)
+
+    unique, counts = np.unique(train_data.labels, return_counts=True)
+    print(unique, counts)
+ 
+
+    for workers in range(0, os.cpu_count() + 1, 2):
+        start_time = time.time()
+        dataloader = DataLoader(dataset, batch_size=32, num_workers=workers, pin_memory=True)
+        for batch in dataloader:
+            pass  # Simulate batch processing
+        print(f"num_workers={workers}, Time taken={time.time() - start_time:.2f}s")
